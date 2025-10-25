98c120a904a1d314edeab7f0765e42d6d270c2a6
zackstromgenk <andhikafathir06@gmail.com>
2025-10-25 22:22:03 +0700

    fix connect

--- laporan.cpp
+++ laporan.cpp
@@ -6,5 +6,5 @@ void menuLaporan() {
     cout << "1. Laporan Simpanan\n";
     cout << "2. Laporan Pinjaman\n";
     cout << "3. Laporan Pembayaran\n";
-    cout << "Fitur masih dalam pengembangan...\n";
+    cout << "test conect...\n";
 }
