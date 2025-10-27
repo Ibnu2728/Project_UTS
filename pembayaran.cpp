983827b51c26f49c71bc22e640c4d0d3bae6a896
ibnu syuhada suffera <ibnusyuhadasuffera@gmail.com>
2025-10-27 07:06:02 +0700

    oyen ubah

--- pembayaran.cpp
+++ pembayaran.cpp
@@ -6,4 +6,5 @@ void menuPembayaran() {
     cout << "1. Bayar Angsuran\n";
     cout << "2. Lihat Riwayat Pembayaran\n";
     cout << "Fitur masih dalam pengembangan...\n";
+    cout << "oyen 123...\n";
 }
